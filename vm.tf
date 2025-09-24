2ac52ec56b5d7a9891d7029a16649f974eefbf1d
litvi <litvin4uk.roman@gmail.com>
2025-09-25 02:15:08 +0300

    Solution

--- vm.tf
+++ vm.tf
@@ -1,61 +1,32 @@
-resource "azurerm_virtual_machine" "main" {
-  name                             = "${var.prefix}-vm"
-  location                         = data.azurerm_resource_group.example.location
-  resource_group_name              = data.azurerm_resource_group.example.name
-  network_interface_ids            = [azurerm_network_interface.main.id]
-  vm_size                          = "Standard_B1ms"
-  delete_os_disk_on_termination    = true
-  delete_data_disks_on_termination = true
-
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
-  }
-  storage_os_disk {
-    name              = "myosdisk1"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-  os_profile {
-    computer_name  = "hostname"
-    admin_username = "testadmin"
-    admin_password = "Password1234!"
-  }
-  os_profile_linux_config {
-    disable_password_authentication = false
+resource "null_resource" "provision_vm" {
+  triggers = {
+    vm_public_ip = data.azurerm_public_ip.main.ip_address
   }
 
   provisioner "file" {
     source      = "./page.html"
-    destination = "/home/testadmin/page.html"
+    destination = "/home/${var.ssh_user}/page.html"
 
     connection {
-      type     = "ssh"
-      user     = "testadmin"
-      password = "Password1234!"
-      host     = azurerm_public_ip.example.ip_address
+      type        = "ssh"
+      host        = self.triggers.vm_public_ip
+      user        = var.ssh_user
+      private_key = file(var.ssh_private_key_path) # або password, якщо використовуєш пароль
     }
   }
 
   provisioner "remote-exec" {
     connection {
-      type     = "ssh"
-      user     = "testadmin"
-      password = "Password1234!"
-      host     = azurerm_public_ip.example.ip_address
+      type        = "ssh"
+      host        = self.triggers.vm_public_ip
+      user        = var.ssh_user
+      private_key = file(var.ssh_private_key_path)
     }
 
     inline = [
       "sudo apt-get update -y",
       "sudo apt-get install -y nginx",
-      "sudo mv /home/testadmin/page.html /var/www/html/index.html",
+      "sudo mv /home/${var.ssh_user}/page.html /var/www/html/index.html",
     ]
-  }
-
-  tags = {
-    environment = "staging"
   }
 }
